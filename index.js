67810848b341935fc88e0d3c8433fb7245fcad20
Ankit-Singla <asingla590@gmail.com>
2020-04-16 16:14:10 +0530

    enables the download of crux as a text file

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 var crux = document.getElementById('crux');
 crux.value = chrome.extension.getBackgroundPage().document.getElementById('crux').value;
+
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
+document.getElementById("download-button").addEventListener("click", function(){
+    download('crux.txt', crux.value);
+    window.close();
+}, false);
