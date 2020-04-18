0c1f807fc98c71998f95147e765a450982f502b5
Ankit-Singla <asingla590@gmail.com>
2020-04-18 23:58:50 +0530

    prevents same text from being copied twice consecutively

--- background.js
+++ background.js
@@ -2,14 +2,22 @@ chrome.extension.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		if (request.event == "copy") {
 			var clipboardAccessor = document.getElementById('clip');
+			
+			// prevents accidentally copying the same piece of text twice
+			var previousPaste = clipboardAccessor.value;
+			clipboardAccessor.value = "";
+
 			clipboardAccessor.focus();
 			document.execCommand('paste');
 			var copiedText = clipboardAccessor.value;
 
+			// currently copied text is same as previously copied text, return
+			if(copiedText.localeCompare(previousPaste) == 0) {
+				return true;
+			}
+
 			var crux = document.getElementById('crux');
 			crux.value += (copiedText+"\n");
-
-			clipboardAccessor.value = "";
 		}
 		sendResponse({});
 	}
