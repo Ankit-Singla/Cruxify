4f9226847d762ee809692eb572aefc8e78635de8
Ankit-Singla <asingla590@gmail.com>
2020-04-18 10:52:50 +0530

    download shelf, crux clear bug fix

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ crux.value = chrome.extension.getBackgroundPage().document.getElementById('crux'
 
 var win = window;
 
-chrome.downloads.setShelfEnabled(false);
-
 document.getElementById("download-button").addEventListener("click", function() {
+	chrome.downloads.setShelfEnabled(false);
+
 	var blob = new Blob([crux.value], {type: "text/plain"});
 	var url = URL.createObjectURL(blob);
 	console.log('url: '+url);
@@ -18,6 +18,7 @@ document.getElementById("download-button").addEventListener("click", function() 
 });
 
 chrome.downloads.onChanged.addListener(function (downloadDelta) {
+	chrome.extension.getBackgroundPage().document.getElementById('crux').value = '';
 	crux.value = '';
 	win.close();
 	chrome.downloads.setShelfEnabled(true);
